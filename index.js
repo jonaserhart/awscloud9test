2f6c773fa0f03071c3d164b119767fe38b0786e2
jonaserhart <jonaserhart@protonmail.com>
2022-10-07 09:37:59 +0000

    added code

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+fs.readFile("./file.json", (error, content) => {
+    if (error) {
+        console.error(error);
+    } else {
+        const json = JSON.parse(content);
+        console.log(`found ${json.services.count} services:`);
+        json.services.forEach((s) => {
+            console.log(`Name: ${s.name}, Description: ${s.description}`) 
+        });
+    }
+});
